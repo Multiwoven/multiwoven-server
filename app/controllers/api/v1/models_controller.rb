4db2a6d7fe157baa243bc0ff82bb89aac6590ea9
Subin T P <subinthattaparambil@gmail.com>
2023-10-29 22:05:29 +0530

    Add core model APIs

--- app/controllers/api/v1/models_controller.rb
+++ app/controllers/api/v1/models_controller.rb
@@ -4,9 +4,17 @@ module Api
   module V1
     class ModelsController < ApplicationController
       include Models
-
-      before_action :set_connector
       attr_reader :connector, :model
+
+      before_action :set_connector, only: %i[create]
+      before_action :set_model, only: %i[show update destroy]
+
+      def index
+        @models = current_workspace
+                  .models.all.page(params[:page] || 1)
+      end
+
+      def show; end
 
       def create
         result = CreateModel.call(
@@ -22,10 +30,33 @@ module Api
         end
       end
 
+      def update
+        result = UpdateModel.call(
+          model:,
+          model_params:
+        )
+
+        if result.success?
+          @model = result.model
+        else
+          render json: { errors: result.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        model.destroy!
+        head :no_content
+      end
+
       private
 
       def set_connector
         @connector = current_workspace.connectors.find(model_params[:connector_id])
+      end
+
+      def set_model
+        @model = current_workspace.models.find(params[:id])
       end
 
       def model_params
