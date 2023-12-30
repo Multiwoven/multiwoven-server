6667a8705253502badb0b6c97f2d9f4d22aea959
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2023-12-30 19:54:46 +0530

    Workspace Users Spec FIX

--- spec/interactors/workspace_users/create_spec.rb
+++ spec/interactors/workspace_users/create_spec.rb
@@ -4,12 +4,12 @@ require "rails_helper"
 
 RSpec.describe WorkspaceUsers::Create, type: :interactor do
   let(:workspace) { create(:workspace) }
-  let(:user_params) { { email: "test@example.com", role: "member" } }
-
+  
   describe ".call" do
     subject(:result) { described_class.call(workspace:, user_params:) }
 
     context "when given valid parameters" do
+      let(:user_params) { { email: "test@example.com", role: "member" } }
       it "succeeds" do
         expect(result).to be_success
       end
@@ -26,19 +26,21 @@ RSpec.describe WorkspaceUsers::Create, type: :interactor do
 
     context "when given invalid parameters" do
       let(:user_params) { { email: "", role: "member" } }
+      
+      before do
+        result
+      end
 
       it "fails" do
         expect(result).to be_failure
       end
 
       it "does not create a WorkspaceUser" do
-        result
-        expect(WorkspaceUser.where(workspace:, role: "member")).to be_empty
+        expect(WorkspaceUser.where(workspace: workspace, role: "member")).to be_empty
       end
 
       it "does not create a User" do
-        result
-        expect(User.where(email: "")).to be_empty
+        expect(User.where(email: user_params[:email])).to be_empty
       end
     end
   end
