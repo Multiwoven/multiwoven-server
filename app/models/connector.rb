c6a4c429162b1df91179718c6358644197244093
Subin T P <subinthattaparambil@gmail.com>
2024-01-03 16:52:33 +0530

    Add connector_name to connector model

--- app/models/connector.rb
+++ app/models/connector.rb
@@ -4,7 +4,11 @@ class Connector < ApplicationRecord
   validates :workspace_id, presence: true
   validates :connector_type, presence: true
   validates :configuration, presence: true
+
+  # User defined name for this connector eg: reporting warehouse
   validates :name, presence: true
+  # Connector name eg: snowflake
+  validates :connector_name, presence: true
 
   enum :connector_type, %i[source destination]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_30_143430) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_03_111636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_143430) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "connector_name"
   end
 
   create_table "models", force: :cascade do |t|
