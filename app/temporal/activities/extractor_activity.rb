ab7636ef2f9a67082de39811890610e56c4425f8
Subin T P <subinthattaparambil@gmail.com>
2024-01-19 00:50:33 +0530

    Implement extractor activity

--- app/temporal/activities/extractor_activity.rb
+++ app/temporal/activities/extractor_activity.rb
@@ -2,8 +2,15 @@
 
 module Activities
   class ExtractorActivity < Temporal::Activity
-    def execute
-      puts "ExtractorActivity.execute"
+    def execute(sync_run)
+      sync_run.status = "in_progress"
+      sync_run.started_at = Time.zone.now
+      sync_run.save!
+
+      # TODO: Select extraction strategy
+      # based on sync mode eg: incremental/full_refresh
+      extractor = ReverseEtl::Extractors::IncrementalDelta.new
+      extractor.read(sync_run.id)
     end
   end
 end
