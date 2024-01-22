92228a3011700a1e64f93f0e06ff24f65beaa9f6
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2024-01-22 16:01:31 +0530

    Added sync serializer - AMS

---
+++ app/serializers/sync_serializer.rb
@@ -0,0 +1,5 @@
+class SyncSerializer < ActiveModel::Serializer
+  attributes :id, :source_id, :destination_id, :model_id, :configuration,
+             :schedule_type, :sync_mode, :sync_interval, :sync_interval_unit,
+             :stream_name, :status
+end
