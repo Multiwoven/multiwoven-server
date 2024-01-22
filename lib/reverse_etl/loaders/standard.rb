6a6dbc30684cf2e72abce9f6b08637c350b8f318
Subin T P <subinthattaparambil@gmail.com>
2024-01-22 19:10:31 +0530

    Support partial retry in loader

--- lib/reverse_etl/loaders/standard.rb
+++ lib/reverse_etl/loaders/standard.rb
@@ -3,7 +3,7 @@
 module ReverseEtl
   module Loaders
     class Standard < Base
-      THREAD_COUNT = 5
+      THREAD_COUNT = 10
       def write(sync_run_id)
         sync_run = SyncRun.find(sync_run_id)
         sync = sync_run.sync
@@ -12,17 +12,25 @@ module ReverseEtl
         transformer = Transformers::UserMapping.new
         client = sync.destination.connector_client.new
 
-        # TODO: Fetch only records with status pending
-        sync_run.sync_records.find_in_batches do |sync_records|
+        sync_run.sync_records.pending.find_in_batches do |sync_records|
+          # track sync record status
+          successfull_sync_records = []
+          failed_sync_records = []
+
           Parallel.each(sync_records, in_threads: THREAD_COUNT) do |sync_record|
             record = transformer.transform(sync, sync_record)
-            report = client.write(sync_config, [record])
-            # TODO: Update status of sync recrod using IN query
-            # if 100% of the batch failed then save the error also to the sync run
-            puts report
+            report = client.write(sync_config, [record]).tracking
+
+            if report.success.zero?
+              failed_sync_records << sync_record.id
+            else
+              successfull_sync_records << sync_record.id
+            end
           rescue StandardError => e
             Rails.logger(e)
           end
+          sync_run.sync_records.where(id: successfull_sync_records).update_all(status: "success") # rubocop:disable Rails/SkipsModelValidations
+          sync_run.sync_records.where(id: failed_sync_records).update_all(status: "failed") # rubocop:disable Rails/SkipsModelValidations
         end
       end
     end
