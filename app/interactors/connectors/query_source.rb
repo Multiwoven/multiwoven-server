bdc259b031a861eeee10e0cc4a294f90aa3dee78
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2024-01-25 20:06:16 +0530

    Implemented connector query API for source reused model query interactor handled invalid SQL exceptions

---
+++ app/interactors/connectors/query_source.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Connectors
+  class QuerySource
+    include Interactor
+
+    def call
+      result = Models::ExecuteQuery.call(
+        connector: context.connector,
+        query: context.query,
+        limit: context.limit
+      )
+      if result["errors"].present?
+        context.fail!(error: "#{result["errors"]}")
+      else
+        context.records = result.records
+      end
+    rescue StandardError => e
+      context.fail!(errors: e.message)
+    end
+
+  end
+end
