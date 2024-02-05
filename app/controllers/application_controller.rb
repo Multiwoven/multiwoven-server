60173a91f316c3363be59492db7c752bcf6dce5d
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2024-02-05 17:42:41 +0530

    Debug Githu action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   include ScriptVault::Tracker
   before_action :authenticate_user!
   around_action :handle_with_exception
-  after_action :event_logger
+  # after_action :event_logger
 
   private
 
