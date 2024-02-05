1b21b049ebaa754c5b8c243094bb3714d37104f3
Subin T P <subinthattaparambil@gmail.com>
2024-02-05 20:17:32 +0530

    Fixes rubocop

--- spec/models/connector_spec.rb
+++ spec/models/connector_spec.rb
@@ -58,11 +58,13 @@ RSpec.describe Connector, type: :model do
   describe "#execute_query" do
     let(:workspace) { create(:workspace) } # Assuming you have factories set up for workspace
     let(:connector) do
-      create(:connector,
-            workspace: workspace,
-            connector_type: :source,
-            connector_name: "snowflake",
-            configuration: { user: "test", password: "password" }) # Adjust attributes as necessary
+      create(
+        :connector,
+        :workspace,
+        connector_type: :source,
+        connector_name: "snowflake",
+        configuration: { user: "test", password: "password" }
+      ) # Adjust attributes as necessary
     end
     let(:client_double) { instance_double("SomeClient") }
     let(:db_connection) { instance_double("SomeDBConnection") }
