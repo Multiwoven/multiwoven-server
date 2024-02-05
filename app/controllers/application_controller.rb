e20e43c67ac7e9bb4449a53881dbc874c1cd2778
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2024-02-05 17:57:34 +0530

    Debug Github action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::API
   include Devise::Controllers::Helpers
   include ExceptionHandler
-  include ScriptVault::Tracker
+  # include ScriptVault::Tracker
   before_action :authenticate_user!
   around_action :handle_with_exception
   # after_action :event_logger
