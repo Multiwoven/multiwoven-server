a62de222ae1a9b91798db985968850479ddfdb34
Subin T P <subinthattaparambil@gmail.com>
2024-02-19 01:18:33 +0530

    Add connector contracts

---
+++ app/contracts/connector_contracts.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+module ConnectorContracts
+  class Index < Dry::Validation::Contract
+    params do
+      optional(:type).filled(:string)
+    end
+
+    rule(:type) do
+      unless !key? || Multiwoven::Integrations::Protocol::ConnectorType.include?(value.downcase)
+        key.failure("invalid type")
+      end
+    end
+  end
+
+  class Show < Dry::Validation::Contract
+    params do
+      required(:id).filled(:integer)
+    end
+  end
+
+  class Create < Dry::Validation::Contract
+    params do
+      required(:connector).hash do
+        required(:name).filled(:string)
+        required(:connector_name).filled(:string)
+        required(:connector_type).filled(:string)
+        required(:configuration).filled(:hash)
+      end
+    end
+
+    rule(connector: :connector_type) do
+      unless Multiwoven::Integrations::Protocol::ConnectorType.include?(value.downcase)
+        key.failure("invalid connector type")
+      end
+    end
+
+    rule(connector: %i[connector_type connector_name]) do
+      if value.first.downcase == Multiwoven::Integrations::Protocol::ConnectorType["source"]
+        unless Multiwoven::Integrations::ENABLED_SOURCES.include?(value.second)
+          key.failure("invalid connector source name")
+        end
+      elsif value.first.downcase == Multiwoven::Integrations::Protocol::ConnectorType["destination"]
+        unless Multiwoven::Integrations::ENABLED_DESTINATIONS.include?(value.second)
+          key.failure("invalid connector destination name")
+        end
+      end
+    end
+  end
+
+  # TODO: Test this
+  class Update < Dry::Validation::Contract
+    params do
+      required(:id).filled(:integer)
+      required(:connector).hash do
+        optional(:name).filled(:string)
+        optional(:connector_name).filled(:string)
+        optional(:connector_type).filled(:string)
+        optional(:configuration).filled(:hash)
+      end
+    end
+
+    rule(connector: :connector_type) do
+      unless !key? || Multiwoven::Integrations::Protocol::ConnectorType.include?(value.downcase)
+        key.failure("invalid connector type")
+      end
+    end
+
+    rule(connector: %i[connector_type connector_name]) do
+      if value.first.downcase == Multiwoven::Integrations::Protocol::ConnectorType["source"]
+        unless Multiwoven::Integrations::ENABLED_SOURCES.include?(value.second)
+          key.failure("invalid connector source name")
+        end
+      elsif value.first.downcase == Multiwoven::Integrations::Protocol::ConnectorType["destination"]
+        unless Multiwoven::Integrations::ENABLED_DESTINATIONS.include?(value.second)
+          key.failure("invalid connector destination name")
+        end
+      end
+    end
+  end
+end
