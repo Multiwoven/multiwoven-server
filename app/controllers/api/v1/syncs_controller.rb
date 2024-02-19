3d6c0f3f09f646d22a7b736fc47671b0854c8bea
Subin T P <subinthattaparambil@gmail.com>
2024-02-19 14:56:16 +0530

    Add sync validations

--- app/controllers/api/v1/syncs_controller.rb
+++ app/controllers/api/v1/syncs_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class SyncsController < ApplicationController
       include Syncs
+      before_action :validate_contract
       before_action :set_sync, only: %i[show update destroy]
       after_action :event_logger
 
