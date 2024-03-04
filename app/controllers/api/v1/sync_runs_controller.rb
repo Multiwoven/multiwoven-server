32d668c0b002093b939bf239e0effed60d1c0378
Afthab <afthab01@gmail.com>
2024-03-04 12:32:58 +0530

    feat: adding contract for controller

--- app/controllers/api/v1/sync_runs_controller.rb
+++ app/controllers/api/v1/sync_runs_controller.rb
@@ -3,12 +3,21 @@
 module Api
   module V1
     class SyncRunsController < ApplicationController
-      skip_before_action :validate_contract
+      before_action :set_sync, only: [:index]
+      attr_reader :sync
+
       def index
-        sync_runs = current_workspace.syncs.find(params[:sync_id]).sync_runs
+        sync_runs = @sync.sync_runs
         sync_runs = sync_runs.where(status: params[:status]) if params[:status].present?
         sync_runs = sync_runs.page(params[:page] || 1)
         render json: sync_runs, status: :ok
+      end
+
+      private
+
+      def set_sync
+        @sync = current_workspace.syncs.find(params[:sync_id])
+        render_error(message: "Sync not found", status: :not_found) unless @sync
       end
     end
   end
--- app/serializers/sync_run_serializer.rb
+++ app/serializers/sync_run_serializer.rb
@@ -4,7 +4,6 @@ class SyncRunSerializer < ActiveModel::Serializer
   attributes :id, :sync_id, :status, :started_at, :finished_at, :duration, :total_query_rows, :total_rows, :successful_rows,
              :failed_rows, :error, :created_at, :updated_at
   def duration
-    byebug
-    object.finished_at - object.started_at
+    object.finished_at && object.started_at ? object.finished_at - object.started_at : nil
   end
 end
