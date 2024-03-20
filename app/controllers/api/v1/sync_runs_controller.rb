a2ea7dcc371b2f734c33785cd41b2a73ad598b6e
afthab vp <afthab01@gmail.com>
2024-03-20 11:26:07 +0530

    fix: sync run api by sync id

--- app/controllers/api/v1/sync_runs_controller.rb
+++ app/controllers/api/v1/sync_runs_controller.rb
@@ -3,7 +3,8 @@
 module Api
   module V1
     class SyncRunsController < ApplicationController
-      before_action :set_sync, only: [:index]
+      before_action :set_sync
+      before_action :set_sync_run, only: [:show]
       attr_reader :sync
 
       def index
@@ -13,11 +14,20 @@ module Api
         render json: sync_runs, status: :ok
       end
 
+      def show
+        render json: @sync_run, status: :ok
+      end
+
       private
 
       def set_sync
         @sync = current_workspace.syncs.find_by(id: params[:sync_id])
         render_error(message: "Sync not found", status: :not_found) unless @sync
+      end
+
+      def set_sync_run
+        @sync_run = @sync.sync_runs.find_by(id: params[:id])
+        render_error(message: "Sync Run not found", status: :not_found) unless @sync_run
       end
     end
   end
